42b6d3459a58277da72e666cb1f8052171a4e674
ochealean <ochealean@gmail.com>
2023-03-21 21:52:16 +0800

    inspect element prevention in js

--- index.js
+++ index.js
@@ -207,4 +207,34 @@ window.onscroll = function() {myFunction()};
 				document.getElementById("mobile_contact").style.color = pick_color;
 			  }
 			  }
+
+        //inspect element prevent
+        document.addEventListener("contextmenu", function(e){
+          e.preventDefault();
+          });
+          document.onkeydown = function(e){
+          if(event.keyCode == 123)
+          {
+            return false;
+          }
+          if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0))
+          {
+            return false;
+          }
+          
+          if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0))
+          {
+            return false;
+          }
+          
+          if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0))
+          {
+            return false;
+          }
+          
+          if(e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0))
+          {
+            return false;
+          }
+          };
 			  
